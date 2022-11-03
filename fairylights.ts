912412f7dcbea7d232e4584190d3cb3ad43a9f32
Metamason1 <110866528+Metamason1@users.noreply.github.com>
2022-11-03 17:31:04 +1100

    Add files via upload

---
+++ fairylights.ts
@@ -0,0 +1,89 @@
+export function Createfairylights(): void { 
+
+    // FAIRYLIGHTS
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animated/fairylights/f1.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animated/fairylights/f2.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animated/fairylights/f3.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animated/fairylights/f4.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animated/fairylights/ff1.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animated/fairylights/ff2.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animated/fairylights/ff3.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animated/fairylights/ff4.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animated/fairylights/ff5.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+}
