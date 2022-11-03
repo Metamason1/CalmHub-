8a7caf1a17bf296dc39e7cdd798090124064330e
Metamason1 <110866528+Metamason1@users.noreply.github.com>
2022-11-03 17:20:13 +1100

    Delete player.ts

--- player.ts
+++
@@ -1,7 +0,0 @@
-export class PlayerState {
-  camera: Camera = Camera.instance
-  feetPos: Vector3 = Camera.instance.feetPosition
-  headPos: Vector3 = Camera.instance.position
-}
-
-export const player = new PlayerState()
