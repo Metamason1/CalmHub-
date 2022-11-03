071081fe65c0d0a6b23e7648b2288171c3f12284
Metamason1 <110866528+Metamason1@users.noreply.github.com>
2022-11-03 17:23:59 +1100

    Add files via upload

---
+++ animals.ts
@@ -0,0 +1,194 @@
+export function Createanimals(): void { 
+    
+/////////////////////////////////////////////////////////////////////// ANIMALS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/triggereddino/triggereddinoblood.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/triggereddino/triggereddino.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/oldbehaviordino/oldbehaviordino.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/chicken/chicken.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/sittingdino/sittingdino.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/yellowdragon/yellowdragonbody.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/bluesleepingdragon/bluedragonhead.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/bluesleepingdragon/bluedragonbody.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/bluesleepingdragon/bluedragontail.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/bluesleepingdragon/bluedragonwing.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/bluesleepingdragon/bluedragonlegs.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/griffin/griffinhead.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/griffin/griffintail.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/griffin/griffinnest.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/griffin/griffinwing.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/griffin/griffinbody.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+//////////////////////////////////////////////////////////////////////////OCTO//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/octopus/sucker1.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/octopus/sucker2.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/octopus/sucker3.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/octopus/sucker4.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
+
+const baseScene = new Entity()
+engine.addEntity(baseScene)
+baseScene.addComponent(new GLTFShape("models/animals/octopus/sucker5.glb"))
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(0.8, 0.8, 0.8)
+})
